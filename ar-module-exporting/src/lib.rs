855860c1c63b80b134d4e634cf85d417df4a074f
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2024-12-31 22:56:01 +0330

    module exporting

---
+++ ar-module-exporting/src/lib.rs
@@ -0,0 +1,98 @@
+// if we make a module public, all struct inside will be exposed as public
+// instead, we can use this syntax to only make module importable from outside
+pub use order::Order;
+pub use product::{Category, Product}; // using this syntax expose Product struct and now we can import it in main
+
+mod product {
+    pub use category::Category; //  re-exporting from submodule to product
+
+    pub struct Product {
+        id: u64,
+        name: String,
+        category: Category,
+        price: f32,
+    }
+
+    impl Product {
+        pub fn new(id: u64, name: String, category: Category, price: f32) -> Product {
+            Product {
+                id,
+                name,
+                category,
+                price,
+            }
+        }
+        pub fn calculate_tax(&self) -> f32 {
+            self.price * 0.1
+        }
+
+        pub fn get_final_price(&self) -> f32 {
+            self.price + self.calculate_tax()
+        }
+    }
+
+    mod category {
+        pub enum Category {
+            Electronic,
+            Home,
+            Sports,
+        }
+    }
+}
+
+pub mod customer {
+    pub struct Customer {
+        id: u64,
+        name: String,
+        email: String,
+    }
+
+    impl Customer {
+        pub fn new(id: u64, name: String, email: String) -> Customer {
+            Customer { id, name, email }
+        }
+    }
+}
+
+mod order {
+    use crate::customer::Customer;
+    use crate::product::Product;
+
+    pub struct Order {
+        id: u64,
+        customer: Customer,
+        products: Vec<Product>,
+    }
+
+    impl Order {
+        pub fn new(id: u64, customer: Customer, products: Vec<Product>) -> Order {
+            Order {
+                id,
+                customer,
+                products,
+            }
+        }
+
+        pub fn calculate_discount(&self) -> f32 {
+            if self.products.len() >= 5 {
+                return 0.1;
+            } else {
+                return 0.0;
+            }
+        }
+
+        pub fn total_bill(&self) -> f32 {
+            let mut total: f32 = 0.0;
+            for p in &self.products {
+                total += p.get_final_price();
+            }
+
+            total -= self.calculate_discount();
+            if total < 0.0 {
+                return 0.0;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ ar-module-exporting/src/main.rs
@@ -0,0 +1,28 @@
+use ar_module_exporting::{Category, Order, Product}; // we can import it because of this line in lib.rs: pub use product::Product
+
+use ar_module_exporting::customer::Customer; // we can use customer::Customer because it is a public module
+fn main() {
+    let p1 = Product::new(
+        1,
+        String::from("Airpods Pro Gen2"),
+        Category::Electronic,
+        129.99,
+    );
+    let p2 = Product::new(
+        1,
+        String::from("MacBook Pro"),
+        Category::Electronic,
+        1390.99,
+    );
+
+    let customer = Customer::new(
+        12,
+        String::from("Mohammad"),
+        String::from("mgh@example.com"),
+    );
+
+    let order = Order::new(88, customer, vec![p1, p2]);
+    let total = order.total_bill();
+
+    println!("Total Bill: {}", total);
+}
